0cc4185b460402a7bf538324c9718761a4137bcc
jakyshan1990 <jakyshan1990@yahoo.com>
2024-10-24 19:23:21 +0300

    gooo

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 
+document.addEventListener("DOMContentLoaded",(event) => {
+
+    const form=document.getElementById("registration-form");
+    const feedbackDiv=document.getElementById("form-feedback");
+
+    form.addEventListener("submit",function(event){
+        event.preventDefault();
+
+    });
+
+});
