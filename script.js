10875bb80dab2346863995505d8db5bdc1f35ef4
jakyshan1990 <jakyshan1990@yahoo.com>
2024-10-27 00:20:06 +0300

    gooow

--- script.js
+++ script.js
@@ -4,16 +4,20 @@ document.addEventListener("DOMContentLoaded",(event) => {
     const form=document.getElementById("registration-form");
     const feedbackDiv=document.getElementById("form-feedback");
 
+const username = document.getElementById('username');
+const userinput = username.value.trim;
+const email = document.getElementById('email');
+const eminput = email.value;
+const password = document.getElementById('password');
+const passinput = password.value.trim;
+let isVaild = true;
+let massages = [];
+
+
+
     form.addEventListener("submit",function(event){
         event.preventDefault();
-        const username=document.getElementById("username");
-        const userinput=username.value.trim;
-        const email=document.getElementById("email");
-        const eminput=email.value.trim;
-        const password=document.getElementById("password");
-        const passinput=password.value.trim;  
-        let isVaild=true;
-        let massages=[0];
+        
 
 
         if(userinput.length<3){isVaild=false;
@@ -23,7 +27,7 @@ document.addEventListener("DOMContentLoaded",(event) => {
             isVaild=false;
             massages=["missing @ or dot",...massages];
         }
-        if(passinput<8){
+        if(passinput.length<8){
             isVaild=false;
             massages=["pass too low",...massages];
         }
