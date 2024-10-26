abe0764ca93f766c71a879af56a00ddcbebe9137
jakyshan1990 <jakyshan1990@yahoo.com>
2024-10-26 17:21:12 +0300

    gooow

--- script.js
+++ script.js
@@ -6,6 +6,38 @@ document.addEventListener("DOMContentLoaded",(event) => {
 
     form.addEventListener("submit",function(event){
         event.preventDefault();
+        const username=document.getElementById("username");
+        const userinput=username.value.trim;
+        const email=document.getElementById("email");
+        const eminput=email.value.trim;
+        const password=document.getElementById("password");
+        const passinput=password.value.trim;  
+        let isVaild=true;
+        let massages=[0];
+
+
+        if(userinput.length<3){isVaild=false;
+            massages=["user name too low",...massages];
+        }
+        if(eminput.includes("@",".")==false){
+            isVaild=false;
+            massages=["missing @ or dot",...massages];
+        }
+        if(passinput<8){
+            isVaild=false;
+            massages=["pass too low",...massages];
+        }
+
+        feedbackDiv.style.display="block";
+        if(isVaild==true){
+            feedbackDiv.textContent="Registration successful!";
+            feedbackDiv.style.color="#28a745";
+        }
+        else if(isVaild==false){
+            massages=["<br>",...massages];
+            feedbackDiv.innerHTML=massages;
+            feedbackDiv.style.color="#dc3545";
+        }
 
     });
 
