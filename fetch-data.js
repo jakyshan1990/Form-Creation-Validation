96118e1d5b7652416b6b8d128d7ff9880b024861
jakyshan1990 <jakyshan1990@yahoo.com>
2024-10-27 04:39:49 +0300

    ftch

--- fetch-data.js
+++ fetch-data.js
@@ -1,5 +1,14 @@
 async function fetchUserData() {
     const apiUrl="https://jsonplaceholder.typicode.com/users";
+    const dataContainer=document.getElementById("api-data");
+ try {
+const response= await fetch(apiUrl);  
+const users=await response.json();  
 
+ } catch (error) {
     
+ }
+    
+
+
 }
