ef86fe07cce95dda49d30b0b660c2bae403584c8
jakyshan1990 <jakyshan1990@yahoo.com>
2024-10-27 13:24:42 +0300

    fteech

--- fetch-data.js
+++ fetch-data.js
@@ -1,21 +1,25 @@
 async function fetchUserData() {
     const apiUrl="https://jsonplaceholder.typicode.com/users";
     const dataContainer=document.getElementById("api-data");
- try {
+ 
+    try {
 const response= await fetch(apiUrl);  
 const users=await response.json();
 
 dataContainer.innerHTML='';
 const userList=document.createElement("ul"); 
 
-users.forEach((elmnt) => {
-const usr=document.createElement("li");
-usr.textContent=elmnt;
-userList.appendChild(usr);
-}); 
+users.forEach(myfunction);
+function myfunction(item){
+    const usr=document.createElement("li");
+    usr.textContent=item;
+    userList.append(usr);
+} 
 
 dataContainer.append(userList);
  } catch (error) {
+    dataContainer.innerHTML='';
+    dataContainer.textContent="Failed to load user data.";
     
  }
     
